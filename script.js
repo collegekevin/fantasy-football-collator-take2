f558b7fb147c6052ad0ef8d4eaa75a7888a2d008
oauit <aidanjade.ward@gmail.com>
2022-10-05 18:45:00 -0600

    wednesday draft

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 })
-const URL = 'http://localhost:3000/players';
+const API = 'http://localhost:3000/players';
 const getAllPlayers = () => {
-  fetch(URL)
+  fetch(API)
   .then(results => results.json())
   .then(json => renderPlayers(json))
 };
@@ -14,45 +14,57 @@ const renderPlayers = players => {
 const createDomPlayer = player => {
   // Step 1 of DOM manipulation
   const tr = document.createElement('tr');
-  const firstName = document.createElement('td');
-  const lastName = document.createElement('td');
+  const name = document.createElement('td');
   const team = document.createElement('td');
-  const rushingYards = document.createElement('td');
-  const receivingYards = document.createElement('td');
-  const touchdowns = document.createElement('td');
+  const projectedPoints = document.createElement('td');
+  const projectedPointsLow = document.createElement('td');
+  const projectedPointsHigh = document.createElement('td');
   const buttonHolder = document.createElement('td');
   const button = document.createElement('button');
   // Step 2 of DOM manipulation
-  firstName.textContent = player.firstName;
+  name.textContent = player.firstName;
   lastName.textContent = player.lastName;
   team.textContent = player.team;
-  rushingYards.textContent = player.rushingYards
-  receivingYards.textContent = player.receivingYards
-  touchdowns.textContent = player.touchdowns
+  projectedPoints.textContent = player.projectedPoints
+  projectedPointsLow.textContent = player.projectedPointsLow
+  projectedPointsHigh.textContent = player.projectedPointsHigh
   button.textContent = 'Delete';
   // Step 3 of DOM manipulation
-  buttonHolder.append(button);
-  tr.append(firstName, lastName, team, rushingYards, receivingYards, touchdowns, buttonHolder);
-  const tableBody = document.getElementById('table-body');
-  tableBody.append(tr);
-  // Add event listener to edit button
-  button.addEventListener('click', () => tr.remove());
-  // function deleteButton(e){
-  //   e.target.parentNode.remove()
-  //}
+  
+  form.addEventListener('submit', e => {
+      e.preventDefault();
+      
+      const player = {
+          name: e.target[0].value,
+          
+      }
+      
+      console.log(player);
+      findPlayer(player)
+      
+  })
+  // buttonHolder.append(button);
+  // tr.append(name, team, projectedPoints, projectedPointsLow, projectedPointsHigh, buttonHolder);
+  // const tableBody = document.getElementById('table-body');
+  // tableBody.append(tr);
+  // // Add event listener to edit button
+  // button.addEventListener('click', () => tr.remove());
+  // // function deleteButton(e){
+  // //   e.target.parentNode.remove()
+  // //}
 };
   // Add event listener to edit button
 const addPlayerInfoToForm = player => {
   // Step 1
   const form = document.getElementById('player-form');
-  const children = form.children;
-  const firstNameInput = children[0];
-  const lastNameInput = children[1];
-  const teamInput = children[2];
+  // const children = form.children;
+  const nameInput = children[0];
+  // const lastNameInput = children[1];
+  // const teamInput = children[2];
   // Step 2
-  firstNameInput.value = player.firstName;
-  lastNameInput.value = player.lastName;
-  teamInput.value = player.team;
+  nameInput.value = player.name;
+  // lastNameInput.value = player.lastName;
+  // teamInput.value = player.team;
 };
 const init = () => {
   getAllPlayers();
